0a673e132107063e5e678addf4fdfd22a42f35e7
Leonardo Sinceri <43849125+TigrottoL9@users.noreply.github.com>
2025-02-28 10:12:43 +0100

    Add project files.

---
+++ Lexer/Lexer.cs
@@ -0,0 +1,44 @@
+﻿
+namespace Lexer;
+
+public class Lexer<TToken>(ReadOnlyMemory<char> input, Rule<TToken> skip, List<Rule<TToken>> rules)
+{
+    private Rule<TToken> Skip { get; init; } = skip;
+    private List<Rule<TToken>> Rules { get; init; } = rules;
+
+    protected ReadOnlyMemory<char> Input { get; set; } = input;
+    protected int Cursor { get; set; } = 0;
+
+    public static Lexer<TToken> From(FileInfo fileInfo, Rule<TToken> skip, List<Rule<TToken>> rules)
+    {
+        string content = File.ReadAllText(fileInfo.FullName);
+        return new(content.AsMemory(), skip, rules);
+    }
+
+    public void AddRule(Rule<TToken> rule) { Rules.Add(rule); }
+
+    public TToken NextToken()
+    {
+        ReadOnlySpan<char> text = Input.Span[Cursor..];
+        bool match = Skip.Match(text, out int length, out _);
+
+        if (match)
+        {
+            text = text[length..];
+            Cursor += length;
+        }
+
+        for (int i = 0; i < Rules.Count; i++)
+        {
+            match = Rules[i].Match(text, out length, out TToken token);
+
+            if (!match) { continue; }
+
+            Cursor += length;
+            return token;
+        }
+
+        throw new Exception();
+    }
+}
+
---
+++ Lexer/Rule.cs
@@ -0,0 +1,49 @@
+﻿
+using System.Buffers;
+using System.Collections;
+
+namespace Lexer;
+
+public abstract class Rule<TToken>
+{
+    public abstract bool Match(ReadOnlySpan<char> text, out int length, out TToken token);
+}
+
+public class SkipRule<TToken> : Rule<TToken>
+{
+    protected ReadOnlyMemory<string> ValuesToSkip { get; set; }
+
+    public SkipRule(string[] valuesToSkip)
+    {
+        string[] values = valuesToSkip.ToArray();
+        Array.Sort(values, (a, b) => a.Length - b.Length);
+
+        if (values[0].Length == 0) { throw new Exception(); }
+
+        ValuesToSkip = values.AsMemory();
+    }
+
+    public override bool Match(ReadOnlySpan<char> text, out int length, out TToken token)
+    {
+        ReadOnlySpan<string> values = ValuesToSkip.Span;
+        length = 0;
+        token = default!;
+
+        for (int i = 0; i < values.Length; i++)
+        {
+            ReadOnlySpan<char> skip = values[i];
+            int offset = skip.Length;
+            if (offset > text.Length) { break; }
+
+            if (skip.SequenceEqual(text[..offset]))
+            {
+                length += offset;
+                i = 0;
+            }
+
+            while (skip.SequenceEqual(text.Slice(length, offset))) { length += offset; }
+        }
+
+        return length > 0;
+    }
+}
